5e116a23806a4d19ab6272ddbe7ea651e9dea9d4
ThakurAnkur <thakur.ankur049@gmail.com>
2022-06-26 08:32:01 +0530

    feat: adding some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login/login-form/login-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found/page-not-found.component';
+import { RegisterFormComponent } from './register/register-form/register-form.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginFormComponent},
+  {path: 'register', component: RegisterFormComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
 import { RegisterModule } from './register/register.module';
 
 @NgModule({
@@ -13,7 +15,6 @@ import { RegisterModule } from './register/register.module';
   ],
   imports: [
     BrowserModule,
-    RegisterModule,
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule
